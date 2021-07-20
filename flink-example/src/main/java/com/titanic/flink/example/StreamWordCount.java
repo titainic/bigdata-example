33db09f0d835fc4440bda168c6ff53cf7b485466
titanic <binend@live.cn>
2021-07-20 22:00:41 +0800

    StreamWordCount

--- flink-example/src/main/java/com/titanic/flink/example/StreamWordCount.java
+++ flink-example/src/main/java/com/titanic/flink/example/StreamWordCount.java
@@ -2,46 +2,39 @@ package com.titanic.flink.example;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
  * 实时计算
- *
  */
 public class StreamWordCount
 {
     public static void main(String[] args) throws Exception
     {
-        //获取执行环节
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
 
-        //设置并行度
-        env.setParallelism(8);
+        DataStream<String> inputStream = env.socketTextStream(host, port);
 
-        //加载数据
-        DataStream<String> inputDataStream = env.readTextFile("flink-example/src/main/resources/hello.txt");
-
-        //类似于spark的flatMap处理
-        DataStream<Tuple2<String, Integer>> resultDataStream = inputDataStream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>()
+        DataStream<Tuple2<String,Integer>> resultStream = inputStream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>()
         {
             public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception
             {
-                // 按空格分词
                 String[] words = s.split(" ");
-                // 遍历所有word，包成二元组输出
+
                 for (String word : words)
                 {
-                    collector.collect(new Tuple2<String, Integer>(word, 1));
+                    collector.collect(new Tuple2<String, Integer>(word,1));
                 }
             }
-        }).keyBy(0).sum(1);
+        }).keyBy(0).sum(1).setParallelism(2).slotSharingGroup("red");
 
-        //打印
-        resultDataStream.print();
-
-        //执行流处理
+        resultStream.print();
         env.execute();
     }
 }
